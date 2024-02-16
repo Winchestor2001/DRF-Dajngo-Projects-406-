4e3e788c687060adeff46d3e5ced845e46a8e1e4
Winchestor2001 <sbexruz2001@gmail.com>
2024-02-16 17:42:30 +0500

    update code and add new endpoint

--- first_api/views.py
+++ first_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 
 
@@ -15,3 +16,24 @@ class HelloApiView(APIView):
         return Response({'result': f"Salom! {name}"}, status=201)
 
 
+class FruitsAPIView(APIView):
+    fruits = ['Qizil Olma', 'Sariq olma', 'Yashil olma', 'Chirigan olma', 'Olmaga oxshagan nok']
+    
+    def get(self, request, *args, **kwargs):
+        return Response({"fruits": self.fruits}, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        fruit_name = request.data.get('fruit_name')
+        self.fruits.append(fruit_name)
+        return Response({"fruits": self.fruits}, status=status.HTTP_201_CREATED)
+    
+    def put(self, request, *args, **kwargs):
+        fruit_index = int(request.data.get('index'))
+        new_fruit = request.data.get('new_fruit')
+
+        if len(self.fruits) > fruit_index:
+            self.fruits[fruit_index] = new_fruit
+            return Response({"fruits": self.fruits}, status=status.HTTP_205_RESET_CONTENT)
+
+        else:
+            return Response({"error": "index xato"}, status=status.HTTP_404_NOT_FOUND)
