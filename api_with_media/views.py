0e3c42c3699e1d6acaa6c25bd6d8341d05af4e4e
Winchestor2001 <sbexruz2001@gmail.com>
2024-02-21 17:36:56 +0500

    update api_with_media

--- api_with_media/views.py
+++ api_with_media/views.py
@@ -1,10 +1,18 @@
 from rest_framework.views import APIView
 from .models import Jora
 from rest_framework.response import Response
+from .serializers import JoraSerializer
+from rest_framework import status
 
 
 class CreateJoraAPIView(APIView):
-    pass
+    
+    def post(self, request):
+        serializer = JoraSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
