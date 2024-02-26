80079ae7baa375af4433eeb6d2e68f459bded712
Winchestor2001 <sbexruz2001@gmail.com>
2024-02-26 18:50:19 +0500

    update

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'api_auth',
 
     'rest_framework',
-    # 'rest_framework.authtoken',
+    'djoser',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -143,5 +144,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.SessionAuthentication',
+    # ]
 }
--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/drf-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('api/v1/', include('first_api.urls')),
     path('api/v2/', include('models_with_api.urls')),
     path('api/v3/', include('api_with_media.urls')),
