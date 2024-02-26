6adbf9cce8d16f333c07d3042f6c7998ed11f129
Winchestor2001 <sbexruz2001@gmail.com>
2024-02-26 14:34:39 +0500

    update

--- nbu_api/serializers.py
+++ nbu_api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Currency
+from datetime import datetime
 
 
 class CurrencySerializer(ModelSerializer):
     class Meta:
         model = Currency
         exclude = ['id']
+    
+    def to_representation(self, instance):
+        redata = super().to_representation(instance)
+        redata['date'] = datetime.strftime(instance.date, '%d-%m-%Y %H:%M:%S')
+        return redata
 
